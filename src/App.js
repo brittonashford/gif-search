1704ede53088d6ce87e82374aeb56b47cc9e5c64
Bret Kitchell <brittonashford@gmail.com>
2022-01-26 22:11:00 -0600

    refactor to use axios instead of fetch

--- src/App.js
+++ src/App.js
@@ -14,14 +14,15 @@ export default class App extends Component {
   } 
 
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ gifs: responseData.data });
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+  .then(response => {
+    this.setState({
+      gifs: response.data.data
+    })
+  })
+  .catch(error => {
+    console.log('Error fetching and parsing data', error);
+  });
   }
 
   render() { 
